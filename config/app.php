e03a0eea456159f9a3efd303646cbd62e29f302d
Pooyan Rajian <p.rajian@rahyabvas.com>
2023-09-05 16:26:36 +0430

    invoice finishهخ

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
          */
 
         App\Services\RahyabSms\RahyabServiceProvider::class,
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
+        MongoDB\Laravel\MongodbServiceProvider::class,
 
 
         /*
